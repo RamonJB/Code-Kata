b9246a7b2ecec91a6171535f313d514faa21dc22
RamonJB <ramonjpbautista@gmail.com>
2019-03-02 01:08:04 -0500

    Edited spacing

--- 6 Kyu/digitalRoot.js
+++ 6 Kyu/digitalRoot.js
@@ -4,7 +4,10 @@
   6 Kyu
   In this kata, you must create a digital root function.
 
-  A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+  A digital root is the recursive sum of all the digits in a number. Given n, 
+  take the sum of the digits of n. If that value has more than one digit, 
+  continue reducing in this way until a single-digit number is produced. 
+  This is only applicable to the natural numbers.
 
   Here's how it works:
 
